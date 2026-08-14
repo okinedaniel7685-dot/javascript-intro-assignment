08ba645dce93fa01ac7d8c928fdf3fac5f650372
okinedaniel7685-dot <okinedaniel7685@gmail.com>
2026-08-14 15:39:53 +0000

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,8 @@
+const name = "Daniel";
+const age = 25;
+const favoriteProgrammingLanguage = "JavaScript";
+
+document.getElementById("output").innerHTML =
+    "Name: " + name + "<br>" +
+    "Age: " + age + "<br>" +
+    "Favorite Programming Language: " + favoriteProgrammingLanguage;
